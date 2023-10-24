5d68ce6ee386cbbf27c8184cc3edb6cc526c7e64
DylanSwif56 <dylanswift56@gmail.com>
2023-10-24 10:40:16 +0100

    Commit 7

---
+++ app_server/models/timetables.js
@@ -0,0 +1,12 @@
+//Database Connection
+const mongoose = require('mongoose');
+
+const timetablesSchema = new mongoose.Schema({
+	location: {type: String,
+			   required: true	
+			},
+	bus_routes: [String],
+	next_bus: String
+	});
+
+mongoose.model('Timetables', timetablesSchema);
